810aef6c4a4a715933eb6ae90cdb767233e51a2a
Mariana Barboza <46385012+Maribarboza@users.noreply.github.com>
2026-04-28 21:39:09 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -8,30 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             cards.forEach(card => {
                 const textoCard = card.textContent.toLowerCase();
-                card.style.display = textoCard.includes(valorBusca) ? "block" : "none";
+                if (textoCard.includes(valorBusca)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
             });
         });
     }
-});
-
-// Aguarda a página carregar
-document.addEventListener('DOMContentLoaded', () => {
-    const campoBusca = document.querySelector('#inputBusca');
-    const cards = document.querySelectorAll('.card-servico');
-
-    // Escuta o que o usuário digita
-    campoBusca.addEventListener('input', () => {
-        const valorBusca = campoBusca.value.toLowerCase();
-
-        cards.forEach(card => {
-            const textoDoCard = card.textContent.toLowerCase();
-            
-            // Se o texto digitado estiver no card, mostra. Se não, esconde.
-            if (textoDoCard.includes(valorBusca)) {
-                card.style.display = "block";
-            } else {
-                card.style.display = "none";
-            }
-        });
-    });
 });
